2fad89f5300de43eb1dc926ac9b6fcdce105fead
Dev-AARon0 <64801098+Dev-AARon0@users.noreply.github.com>
2021-04-15 23:53:09 -0700

    PR6

--- Extension/content.js
+++ Extension/content.js
@@ -1,1 +1,19 @@
-alert("Alerts are alarming.")
+
+//alert("Alerts are somewhat alarming.")
+
+function afterNavigate() {
+    if ('/watch' === location.pathname) {
+        alert('Watch page!');
+    }
+    if('/channel/' === location.pathname){
+        alert('Channel page!')
+    }
+}
+(document.body || document.documentElement).addEventListener('transitionend',
+  function(/*TransitionEvent*/ event) {
+    if (event.propertyName === 'width' && event.target.id === 'progress') {
+        afterNavigate();
+    }
+}, true);
+// After page load
+setTimeout(afterNavigate(), 50000)
