5ae37c933766a0f0a75b6203c59422a2f4bd9f21
Dev-AARon0 <64801098+Dev-AARon0@users.noreply.github.com>
2021-05-13 21:42:27 -0700

    Pr8

--- Extension/content.js
+++ Extension/content.js
@@ -4,7 +4,15 @@ setTimeout(afterNavigate(), 50000)
 function afterNavigate() {
     console.log(location.pathname);
     if ('/watch' === location.pathname) {
-        alert('Watch page!');
+       // alert("https://socialblade.com/");
+        //alert(document.getElementById("upload-info").innerHTML);
+        if (window.confirm('Click "ok" to open channel\'s social blade in new tab.')) 
+        {
+            var a =document.createElement('a');
+            a.setAttribute('target', '_blank')
+            a.href = 'https://socialblade.com/'
+            a.click()
+        }
     }
     var folder = location.pathname;
     folder = folder.substr(0, folder.lastIndexOf('/'));
@@ -12,4 +20,8 @@ function afterNavigate() {
     if('/channel' === folder || '/c' === folder || '/user' === folder){
         alert('Channel page!');
     }
+}
+
+function vidSocial() {
+
 }
